2f1e05be95b435c9f8294cae4ed87e5cdacf6320
Suvetha P <suvetha.p@mphasis.com>
2026-04-08 15:28:24 +0530

    Add new folders to policies_api branch

---
+++ Backend/src/main/java/com/project/entities/Policy.java
@@ -0,0 +1,33 @@
+package com.project.entities;
+
+import com.project.enums.Type;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "policies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Policy {
+
+    @Id
+    private long id;
+
+    @Column(length = 50)
+    private String name;
+    private Type type;
+
+    private double premium;
+    private double coverageAmount;
+    private  int waitingPeriod;
+    private String benifits;
+    private double claimLimit;
+
+    @OneToMany(mappedBy = "policy")
+    private List<UserPolicy> userPolicies;
+}
