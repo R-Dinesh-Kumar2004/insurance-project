bacfa4c1c94e2ed8108577554902ccf488f6b4c0
R-Dinesh-Kumar2004 <dineshkumarraja2004@gmail.com>
2026-04-10 14:43:14 +0530

    after working

--- insurence-project/Backend/src/main/java/com/project/InsuranceComparisonApplication.java
+++ insurence-project/Backend/src/main/java/com/project/InsuranceComparisonApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsuranceComparisonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InsuranceComparisonApplication.class, args);
+        SpringApplication.run(InsuranceComparisonApplication.class, args);
 	}
 }
